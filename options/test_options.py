e99285f5d45a7e2c6c9b84522de860dd0cfbe561
shikhar-scs <shikhar9a@gmail.com>
2019-12-27 09:25:47 +0530

    test tweakings

--- options/test_options.py
+++ options/test_options.py
@@ -11,5 +11,9 @@ class TestOptions(BaseOptions):
         self.parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
         self.parser.add_argument('--how_many', type=int, default=300, help='how many test images to run')        
         self.parser.add_argument('--use_real_img', action='store_true', help='use real image for first frame')
-        self.parser.add_argument('--start_frame', type=int, default=0, help='frame index to start inference on')        
+        self.parser.add_argument('--start_frame', type=int, default=0, help='frame index to start inference on')
+
+        # for oulu
+        self.parser.add_argument('--source_view', type=int, default=1, help='the source view which is given as input')
+        self.parser.add_argument('--target_view', type=int, default=3, help='the target view which is expected as output')
         self.isTrain = False
